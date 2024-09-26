73a539a2fd70cc2881eaec5d636d35249155e4cd
Hritvik1211 <146126198+Hritvik1211@users.noreply.github.com>
2024-09-26 12:24:40 +0530

    Update experiment_4.py

--- experiment_4.py
+++ experiment_4.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-# Step 1: Import
+# Import
 data = pd.read_csv('dataset/student-dataset.csv')
 
-# Step 2: Show the details of the dataset
+# Details of the dataset
 print("Number of Rows and Columns:", data.shape)
 print("First Five Rows:")
 print(data.head())
@@ -11,7 +11,7 @@ print("Dataset Size (Total Elements):", data.size)
 print("Number of Missing Values:")
 print(data.isnull().sum())
 
-# Step 3: Sum, Average, Min, and Max from numerical columns
+# Sum, Average, Min, and Max from numerical columns
 print("Sum of Numerical Columns:")
 print(data.sum(numeric_only=True))
 
@@ -24,5 +24,5 @@ print(data.min(numeric_only=True))
 print("Maximum Values in Numerical Columns:")
 print(data.max(numeric_only=True))
 
-# Step 4: Export the data (saving to CSV file)
+# Export the data (saving to CSV file)
 data.to_csv('exported_data.csv', index=False)
